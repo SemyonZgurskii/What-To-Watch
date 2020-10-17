88c2ad1bc361b3820d62f95e8050715849776e41
Semyon <saymon221090@gmail.com>
2020-10-17 17:51:40 +0300

    api.ts создаёт модуль для создания запросов к серверу

---
+++ src/api.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const Error: {[key: string]: number} = {
+  UNAUTHORIZED: 401,
+}
+
+function createAPI(onUnauthorized: () => void): object {
+  const api = axios.create({
+    baseURL: `https://4.react.pages.academy/wtw`,
+    timeout: 5000,
+    withCredentials: true,
+  });
+
+  function onSuccess(response) {
+    return response;
+  }
+
+  function onFail(err) {
+    const {response} = err;
+
+    if (response.status === Error.UNAUTHORIZED) {
+      onUnauthorized();
+
+      throw err;
+    }
+
+    throw err;
+  }
+
+  api.interceptors.response.use(onSuccess, onFail);
+
+  return {};
+}
+
+export default createAPI;
