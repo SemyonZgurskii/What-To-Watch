56dc6884ec7f4014a8863ed5f07b39792859c062
Semyon <saymon221090@gmail.com>
2020-11-13 20:11:46 +0300

    package.json: добавляет команды для работы с проектом. Исправляет проблемы с вебпаком

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+// TODO: доделать сбору (typescript и т.д)
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+import App from './components/app/app.tsx';
+
+ReactDom.render(
+  <App/>,
+  document.getElementById('root')
+)
