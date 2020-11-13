ea723caa5b3f25ceaed89f0b6f69a09ada3f22d5
Semyon <saymon221090@gmail.com>
2020-11-13 20:25:59 +0300

    index.tsx : добавляет redux store, оборачивает App в Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 // TODO: доделать сбору (typescript и т.д)
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
 
-import App from './components/app/app.tsx';
+import App from './components/app/app';
+import {createAPI} from './api';
+import {reducer, Operation} from './reducer/data/data';
+
+
+const api = createAPI(() => console.log('wtf'));
+const store = createStore(
+  reducer,
+  applyMiddleware(thunkMiddleware.withExtraArgument(api))
+);
 
 ReactDom.render(
-  <App/>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 )
