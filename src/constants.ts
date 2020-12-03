9f530d51f0cf773a7e3f69841796d808afdc6593
Semyon <saymon221090@gmail.com>
2020-12-03 21:48:57 +0300

    reducer.tsx : добавляет app редьюсер к общему редьюсеру

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,15 @@
 export const BASIC_MOVIES_COUNT: number = 8;
 export const MOVIES_PER_CLICK: number = 8;
 export const MAX_GENRES_COUNT: number = 9;
+export enum Genre {
+  AllGenres = "All genres",
+  Comedies = "comedies",
+  Crime = "crime",
+  Documentary = "documentary",
+  Dramas = "dramas",
+  Horror = "horror",
+  KidsAndFamily = "kids & family",
+  Romance = "romance",
+  SciFi = "sci-fi",
+  Thrillers = "thrillers",
+}
