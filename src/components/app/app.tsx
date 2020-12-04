4329399d90adf82b0afa27a516256ed20d348c87
Semyon <saymon221090@gmail.com>
2020-12-04 15:40:52 +0300

    app.tsx : встраивает компонент GenreMenu

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,19 +3,26 @@ import MoviesList from "../movies-list/movies-list";
 import withShowMoreButton from "../../hocs/with-show-more-button/with-show-more-button";
 import {MoviesData, GlobalState, Movie} from '../../types';
 import {connect} from 'react-redux';
-import {getMoviesData} from "../../reducer/data/selector";
+import {getGenres, getMoviesData} from "../../reducer/data/selector";
+import {getActiveGenre} from "../../reducer/app/selector";
+import {ActionCreator} from "../../reducer/app/app";
+import GenreMenu from "../genre-menu/genre-menu";
+import {Genre} from "../../constants";
 
 const MoviesListWrapped = withShowMoreButton<Movie>(MoviesList)
 
 interface Props {
   moviesData: MoviesData;
+  genres: Genre[],
+  activeGenre: Genre,
+  setActiveGenre: (Genre) => void,
 }
 
 class App extends React.PureComponent<Props, {}> {
   static defaultProps = {moviesData: null};
 
   render() {
-    const {moviesData} = this.props;
+    const {moviesData, activeGenre, genres, setActiveGenre} = this.props;
 
     return (
       <>
@@ -79,44 +86,20 @@ class App extends React.PureComponent<Props, {}> {
           <section className="catalog">
             <h2 className="catalog__title visually-hidden">Catalog</h2>
 
-            <ul className="catalog__genres-list">
-              <li className="catalog__genres-item catalog__genres-item--active">
-                <a href="#" className="catalog__genres-link">All genres</a>
-              </li>
-              <li className="catalog__genres-item">
-                <a href="#" className="catalog__genres-link">Comedies</a>
-              </li>
-              <li className="catalog__genres-item">
-                <a href="#" className="catalog__genres-link">Crime</a>
-              </li>
-              <li className="catalog__genres-item">
-                <a href="#" className="catalog__genres-link">Documentary</a>
-              </li>
-              <li className="catalog__genres-item">
-                <a href="#" className="catalog__genres-link">Dramas</a>
-              </li>
-              <li className="catalog__genres-item">
-                <a href="#" className="catalog__genres-link">Horror</a>
-              </li>
-              <li className="catalog__genres-item">
-                <a href="#" className="catalog__genres-link">Kids & Family</a>
-              </li>
-              <li className="catalog__genres-item">
-                <a href="#" className="catalog__genres-link">Romance</a>
-              </li>
-              <li className="catalog__genres-item">
-                <a href="#" className="catalog__genres-link">Sci-Fi</a>
-              </li>
-              <li className="catalog__genres-item">
-                <a href="#" className="catalog__genres-link">Thrillers</a>
-              </li>
-            </ul>
+            {moviesData &&
+              <GenreMenu
+                genres={genres}
+                setActiveGenre={setActiveGenre}
+                // activeGenre={activeGenre}
+              />
+            }
 
             {moviesData &&
               <MoviesListWrapped basicData={moviesData}/>
             }
 
           </section>
+
 
           <footer className="page-footer">
             <div className="logo">
@@ -140,7 +123,17 @@ class App extends React.PureComponent<Props, {}> {
 function mapStateToProps(state: GlobalState) {
   return {
     moviesData: getMoviesData(state),
+    activeGenre: getActiveGenre(state),
+    genres: getGenres(state),
   }
 }
 
-export default connect(mapStateToProps)(App);
+function mapDispatchToProps(dispatch) {
+  return {
+    setActiveGenre(genre) {
+      dispatch(ActionCreator.setActiveGenre(genre));
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
