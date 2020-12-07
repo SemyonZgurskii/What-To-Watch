90c6adfb69ba2f9b3808cd4f8ce9d5848a3d18b9
Semyon <saymon221090@gmail.com>
2020-12-07 15:31:10 +0300

    promo.tsx |:| заменяет моковые данные на динамические

--- src/components/promo/promo.tsx
+++ src/components/promo/promo.tsx
@@ -1,42 +1,63 @@
 import * as React from "react";
 import history from "../../history";
 import {AppRoute} from "../../constants";
+import {Movie} from "../../types";
 
-function Promo() {
+interface Props {
+  promoMovie: Movie,
+  children: React.ReactNode,
+}
+
+function Promo(props: Props) {
+  const {promoMovie, children} = props;
+  const {posterImage, releaseDate, genre, name, backgroundImage} = promoMovie;
+
   return (
-    <div className="movie-card__info">
-      <div className="movie-card__poster">
-        <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218"
-             height="327"/>
+    <section className="movie-card">
+      <div className="movie-card__bg">
+        <img src={backgroundImage} alt={name}/>
       </div>
 
-      <div className="movie-card__desc">
-        <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
-        <p className="movie-card__meta">
-          <span className="movie-card__genre">Drama</span>
-          <span className="movie-card__year">2014</span>
-        </p>
+      <h1 className="visually-hidden">WTW</h1>
 
-        <div className="movie-card__buttons">
-          <button
-            onClick={() => history.push(AppRoute.PLAYER)}
-            className="btn btn--play movie-card__button"
-            type="button"
-          >
-            <svg viewBox="0 0 19 19" width="19" height="19">
-              <use xlinkHref="#play-s"></use>
-            </svg>
-            <span>Play</span>
-          </button>
-          <button className="btn btn--list movie-card__button" type="button">
-            <svg viewBox="0 0 19 20" width="19" height="20">
-              <use xlinkHref="#add"></use>
-            </svg>
-            <span>My list</span>
-          </button>
+      {children}
+
+      <div className="movie-card__wrap">
+        <div className="movie-card__info">
+          <div className="movie-card__poster">
+            <img src={posterImage} alt={`${name} poster`} width="218"
+                 height="327"/>
+          </div>
+
+          <div className="movie-card__desc">
+            <h2 className="movie-card__title">{name}</h2>
+            <p className="movie-card__meta">
+              <span className="movie-card__genre">{genre}</span>
+              <span className="movie-card__year">{releaseDate}</span>
+            </p>
+
+            <div className="movie-card__buttons">
+              <button
+                onClick={() => history.push(AppRoute.PLAYER)}
+                className="btn btn--play movie-card__button"
+                type="button"
+              >
+                <svg viewBox="0 0 19 19" width="19" height="19">
+                  <use xlinkHref="#play-s"></use>
+                </svg>
+                <span>Play</span>
+              </button>
+              <button className="btn btn--list movie-card__button" type="button">
+                <svg viewBox="0 0 19 20" width="19" height="20">
+                  <use xlinkHref="#add"></use>
+                </svg>
+                <span>My list</span>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 
