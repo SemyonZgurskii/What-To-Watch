d9d5093711f4dc6c98c186296f7ba3f468344331
Semyon <saymon221090@gmail.com>
2020-12-15 21:07:19 +0300

    убирает лишний код

--- src/reducer/name-space.ts
+++ src/reducer/name-space.ts
@@ -1,11 +1,7 @@
-// const NameSpace: {[key: string]: string} = {
-//   APP: "app",
-//   DATA: "data",
-// }
-
 enum NameSpace {
   APP = "app",
   DATA = "data",
+  USER = "user",
 }
 
 export default NameSpace;
