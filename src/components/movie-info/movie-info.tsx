1ed96d6120e5d3beff8cd9a0befb7fa716597888
Semyon <saymon221090@gmail.com>
2020-12-15 16:26:05 +0300

    добавляет Link на главную страницу

--- src/components/movie-info/movie-info.tsx
+++ src/components/movie-info/movie-info.tsx
@@ -108,11 +108,13 @@ function MovieInfo(props: Props) {
 
         <footer className="page-footer">
           <div className="logo">
-            <a href="main.html" className="logo__link logo__link--light">
+            <Link href="main.html" className="logo__link logo__link--light"
+                  to={AppRoute.MAIN}
+            >
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
               <span className="logo__letter logo__letter--3">W</span>
-            </a>
+            </Link>
           </div>
 
           <div className="copyright">
